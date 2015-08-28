9214104ba6b106a697f6a1cdc811ec7714875562
Juan Arce <juanjo.arce7456@gmail.com>
2015-08-28 18:37:41 -0300

    Refactorizacion de codigo anulada debido a incorrecto funcionamiento, se realizara en el futuro

--- ControlEmbebidoPlataforma.ino
+++ ControlEmbebidoPlataforma.ino
@@ -1,4 +1,49 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
+// 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+//
+// Changelog:
+//      2013-05-08 - added seamless Fastwire support
+//                 - added note about gyro calibration
+//      2012-06-21 - added note about Arduino 1.0.1 + Leonardo compatibility error
+//      2012-06-20 - improved FIFO overflow handling and simplified read process
+//      2012-06-19 - completely rearranged DMP initialization code and simplification
+//      2012-06-13 - pull gyro and accel data from FIFO packet instead of reading directly
+//      2012-06-09 - fix broken FIFO read sequence and change interrupt detection to RISING
+//      2012-06-05 - add gravity-compensated initial reference frame acceleration output
+//                 - add 3D math helper file to DMP6 example sketch
+//                 - add Euler output and Yaw/Pitch/Roll output formats
+//      2012-06-04 - remove accel offset clearing for better results (thanks Sungon Lee)
+//      2012-06-01 - fixed gyro sensitivity to be 2000 deg/sec instead of 250
+//      2012-05-30 - basic DMP initialization working
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2012 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
 #include <PID_v1.h>
+#include <Servo.h>
+#include <math.h>
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
@@ -11,14 +56,13 @@
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
-#include <Servo.h>
-#include "stewartmath.h"
-#include "servoStewart.h"
-#define LED_PIN 13 
+
+
+#define LED_PIN 13 // (Arduino is 13)
 #define SAMPLE_TIME 50
 #define MAX_YPR_OUTPUT 180
 #define MIN_YPR_OUTPUT -180
-#define NUM_OF_DATA_TO_SEND 12
+#define N_SERVOS 6
 #define DATA_RATE_DIV 5
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
@@ -52,9 +96,9 @@ float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gra
 double yawSP, yawIn, yawOut,pitchSP,pitchIn,pitchOut,rollSP,rollIn,rollOut;
 
 //Specify the links and initial tuning parameters
-double yawKp=0.05, yawKi=0, yawKd=0;
-double pitchKp=0.05, pitchKi=0, pitchKd=0;
-double rollKp=0.05, rollKi=0, rollKd=0;
+double yawKp=0.1, yawKi=0, yawKd=0;
+double pitchKp=0.1, pitchKi=0, pitchKd=0;
+double rollKp=0.1, rollKi=0, rollKd=0;
 PID yawPID(&yawIn, &yawOut, &yawSP, yawKp, yawKi, yawKd, DIRECT);
 PID pitchPID(&pitchIn, &pitchOut, &pitchSP, pitchKp, pitchKi, pitchKd, DIRECT);
 PID rollPID(&rollIn, &rollOut, &rollSP, rollKp, rollKi, rollKd, DIRECT);
@@ -63,24 +107,55 @@ float yawActual,pitchActual,rollActual, yawActRad, pitchActRad, rollActRad, yawC
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 boolean mpuCalibrated=false;
 float yawAnterior=0;
+enum quadrant{FIRST,SECOND};
+struct platformServos{
+  Servo servo;
+  quadrant quad;
+};
 
-
-servoStewart Servos;
+//Servo servos[6];
+platformServos servos [6];
 
 int pidOn=0;
 String message;
 char messageData[12][10];  
 int loopCounter= 0;
-
-
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
-float x=101,
-      y=101,
-      z=230;
+const float MMTOCMCONV = 1;
+float A1x=8.66/MMTOCMCONV     ,   A1y=68.21/MMTOCMCONV   ,
+      A2x=45.16/MMTOCMCONV    ,   A2y=5/MMTOCMCONV       ,
+      A3x=230.5/MMTOCMCONV    ,   A3y=5/MMTOCMCONV       ,
+      A4x=267/MMTOCMCONV      ,   A4y=68.21/MMTOCMCONV   ,
+      A5x=174.33/MMTOCMCONV   ,   A5y=233.71/MMTOCMCONV  ,
+      A6x=101.33/MMTOCMCONV   ,   A6y=233.71/MMTOCMCONV  ;
       
+float B1x=-81.5/MMTOCMCONV    ,   B1y=-38.4/MMTOCMCONV   ,
+      B2x=-74/MMTOCMCONV      ,   B2y=-51.3/MMTOCMCONV   ,
+      B3x=74/MMTOCMCONV       ,   B3y=-51.3/MMTOCMCONV   ,
+      B4x=81.5/MMTOCMCONV     ,   B4y=-38.4/MMTOCMCONV   ,
+      B5x=7.5/MMTOCMCONV      ,   B5y=89.8/MMTOCMCONV    ,
+      B6x=-7.5/MMTOCMCONV     ,   B6y=89.8/MMTOCMCONV    ;
+     
+//float B=22 / MMTOCMCONV; //longitud corta de las patas
+//float C=234 / MMTOCMCONV; // longitud larga
+float B=54 / MMTOCMCONV; //longitud corta de las patas
+float C=246 / MMTOCMCONV;//long larga
+
+float x=101 / MMTOCMCONV,
+      y=113 / MMTOCMCONV,
+      z=230 / MMTOCMCONV;
+      
+float terminos[N_SERVOS][3];
+float terminoA,terminoB,terminoC,terminoD,terminoE,terminoF;
+
 float motorAng[N_SERVOS];
+
+int minAngPrimerCuadrante=10;
+int maxAngPrimerCuadrante=90;
+int minAngSegundoCuadrante=90;
+int maxAngSegundoCuadrante=170;
 int contFIR;
 float yawFIR,pitchFIR,rollFIR;
 boolean debugFlag=false;
@@ -148,14 +223,14 @@ void setup() {
         mpuIntStatus = mpu.getIntStatus();    
 
         pidSetupConfigurations();
-        Servos.servosSetupConfigurations();
+        servosSetupConfigurations();
 
 }
 
 void loop() {
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
-    
+
     // wait for MPU interrupt or extra packet(s) available
     while (!mpuInterrupt && fifoCount < packetSize) {
         if(Serial.available()){ 
@@ -205,10 +280,13 @@ void loop() {
              yawActual=yawIn;
              rollActual=rollIn;
              pitchActual=pitchIn;
-             Servos.attachServos(2);
-             xyz_yprToServoAng(x,y,z,yawSP,pitchSP,rollSP,motorAng);                    
-             Servos.writeToServos(motorAng);
-             Serial.println("/Calibrado");
+                for(int j=0;j<6;j++){
+                  pinMode(j+2, OUTPUT); 
+                  servos[j].servo.attach(j+2);
+                }
+                yprToServoAng(yawSP,pitchSP,rollSP,motorAng);                    
+                writeToServos();
+            Serial.println("/Calibrado");
            }else if(mpuCalibrated){
                    if(pidOn==1){
                         yawPID.Compute();
@@ -217,8 +295,14 @@ void loop() {
                         //yawActual+=yawOut;
                         pitchActual+=pitchOut;
                         rollActual+=rollOut;
-                        xyz_yprToServoAng(x,y,z,yawActual,pitchActual,rollActual,motorAng);                    
-                        Servos.writeToServos(motorAng);
+                        yprToServoAng(-yawActual,-pitchActual,rollActual,motorAng);                    
+                        writeToServos();
+                   }else{
+                      yawActual=yawIn;
+                      rollActual=rollIn;
+                      pitchActual=pitchIn;
+                      yprToServoAng(-yawSP,-pitchSP,rollSP,motorAng);  
+                      writeToServos();
                    }
                    if(loopCounter%DATA_RATE_DIV==0){
                       sendData();
@@ -255,7 +339,70 @@ void pidSetupConfigurations(){
         rollSP=0;
 }
 
+void servosSetupConfigurations(){
+   servos[0].quad=SECOND;  
+   servos[1].quad=FIRST;  
+   servos[2].quad=SECOND;  
+   servos[3].quad=FIRST;  
+   servos[4].quad=SECOND;  
+   servos[5].quad=SECOND;  
+}
 
+
+void yprToServoAng(float yaw, float pitch, float roll,float *motorAngles){
+                float alpha= yaw*(PI/180),
+                      beta= pitch*(PI/180),
+                      gamma= roll*(PI/180);    
+                terminoA= cos(alpha)*cos(gamma)+sin(alpha)*sin(beta)*sin(gamma);
+                terminoB= cos(alpha)*sin(beta)*sin(gamma)-sin(alpha)*cos(gamma);
+                terminoC= sin(alpha)*cos(beta);
+                terminoD= cos(alpha)*cos(beta);
+                terminoE= sin(alpha)*sin(beta)*cos(gamma)-cos(alpha)*sin(gamma);
+                terminoF= sin(alpha)*sin(gamma)+cos(alpha)*sin(beta)*cos(gamma);
+                terminos[0][0]= (B2x * terminoA + B2y * terminoB + x - A2x);
+                terminos[0][1]= (B2x * terminoC + B2y * terminoD + y - A2y);
+                terminos[0][2]= (B2x * terminoE + B2y * terminoF + z);
+                terminos[1][0]= (B3x * terminoA + B3y * terminoB + x - A3x);
+                terminos[1][1]= (B3x * terminoC + B3y * terminoD + y - A3y);
+                terminos[1][2]= (B3x * terminoE + B3y * terminoF + z);
+                terminos[2][0]= (B4x * terminoA + B4y * terminoB + x - A4x);
+                terminos[2][1]= (B4x * terminoC + B4y * terminoD + y - A4y);
+                terminos[2][2]= (B4x * terminoE + B4y * terminoF + z);
+                terminos[3][0]= (B5x * terminoA + B5y * terminoB + x - A5x);
+                terminos[3][1]= (B5x * terminoC + B5y * terminoD + y - A5y);
+                terminos[3][2]= (B5x * terminoE + B5y * terminoF + z);
+                terminos[4][0]= (B6x * terminoA + B6y * terminoB + x - A6x);
+                terminos[4][1]= (B6x * terminoC + B6y * terminoD + y - A6y);
+                terminos[4][2]= (B6x * terminoE + B6y * terminoF + z);
+                terminos[5][0]= (B1x * terminoA + B1y * terminoB + x - A1x);
+                terminos[5][1]= (B1x * terminoC + B1y * terminoD + y - A1y);
+                terminos[5][2]= (B1x * terminoE + B1y * terminoF + z);
+                
+                float feetLengths[N_SERVOS];             
+                for (int k=0;k<N_SERVOS;k++){
+                  feetLengths[N_SERVOS-k-1] = sqrt ( pow(terminos[k][0],2) + pow(terminos[k][1],2) + pow(terminos[k][2],2) );           
+                }
+                
+                for (int k=0;k<N_SERVOS;k++){
+                  motorAngles[k] = 90- (acos((B*B + feetLengths[k]*feetLengths[k] - C*C) / (2*B*feetLengths[k])))* (180/PI);
+                  if(isnan(motorAngles[k])){motorAngles[k]=90;}
+                  if(servos[k].quad == SECOND){
+                    motorAngles[k]=180-motorAngles[k];
+
+                  }
+                }
+}
+
+void  writeToServos(){
+  for (int k=0;k<N_SERVOS;k++){                 
+    //Serial.print("m");Serial.print(k+1);Serial.print(":\t");Serial.println(motorAng[k]);
+     if(servos[k].quad == SECOND && (motorAng[k]<=minAngSegundoCuadrante)){motorAng[k]=minAngSegundoCuadrante;}
+     else if(servos[k].quad == SECOND && (motorAng[k]>=maxAngSegundoCuadrante)){motorAng[k]=maxAngSegundoCuadrante;}
+     else if(servos[k].quad == FIRST && (motorAng[k]<=minAngPrimerCuadrante)){motorAng[k]=minAngPrimerCuadrante;}
+     else if(servos[k].quad == FIRST && (motorAng[k]>=maxAngPrimerCuadrante)){motorAng[k]=maxAngPrimerCuadrante;}
+           servos[k].servo.write(motorAng[k]);
+   }
+}
 
 void sendData(){  
     dtostrf(x,1,2,messageData[0]);
@@ -274,8 +421,8 @@ void sendData(){
     dtostrf(motorAng[4],1,2,messageData[10]);
     dtostrf(motorAng[5],1,2,messageData[11]);
     message="(";
-    for (int i=0;i<NUM_OF_DATA_TO_SEND;i++){
-      if(i==NUM_OF_DATA_TO_SEND-1){
+    for (int i=0;i<12;i++){
+      if(i==11){
          message=message+ (String) messageData[i]+")";
       }else{
          message=message+ (String) messageData[i]+",";
@@ -306,6 +453,9 @@ void receiveData(){
                 pitchPID.SetTunings(pitchKp, pitchKi, pitchKd);
                 rollPID.SetTunings(rollKp, rollKi, rollKd);
                 yawOut=pitchOut=rollOut=0;
+                yawActual=yawIn;
+                rollActual=rollIn;
+                pitchActual=pitchIn;
                 //yprToServoAng(yawSP,pitchSP,rollSP,motorAng);                    
                 //writeToServos();
           }
